1708bdfac11c48d14b2439283a86c9e748d0a3cc
mcombs289 <maddy.combs4@gmail.com>
2022-06-23 16:13:01 -0400

    hello

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const morgan = require('morgan')
+const layout = require('./views/layout')
+const app = express()
+app.use(morgan("dev"))
+app.use(express.static(__dirname + "/public"))
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.send(layout())
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    // console.log("hello")
+})
